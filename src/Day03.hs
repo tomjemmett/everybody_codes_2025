482a7447bb58f6073dbc52222d96a7ddb4948d07
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-07 14:49:42 +0000

    slight refactor

--- src/Day03.hs
+++ src/Day03.hs
@@ -7,14 +7,17 @@ import ECSolution (Solution, getInput, makeSolution, runDay)
 day03 :: String -> IO (Int, Int, Int)
 day03 = getInput 3 part1 part2 part3
 
+parseInput :: String -> [Int]
+parseInput = sort . commaSeparatedInts
+
 part1 :: String -> Int
-part1 = sum . nub . sort . commaSeparatedInts
+part1 = sum . nub . parseInput
 
 part2 :: String -> Int
-part2 = sum . take 20 . nub . sort . commaSeparatedInts
+part2 = sum . take 20 . nub . parseInput
 
 part3 :: String -> Int
-part3 = go . sort . commaSeparatedInts
+part3 = go . parseInput
   where
     go [] = 0
     go xs = 1 + go (concatMap tail $ group xs)
