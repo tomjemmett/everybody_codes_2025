e78c8f74368384bc63943bc984ce7f017a7a1575
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-07 22:06:31 +0000

    fixes day 2 tests

--- test/Day02Spec.hs
+++ test/Day02Spec.hs
@@ -6,8 +6,8 @@ spec :: Spec
 spec = describe "Day 2" $ do
   it "Sample" $ do
     s <- day02 "sample"
-    s `shouldBe` ("[357,862]", 4076, 40695)
+    s `shouldBe` ("[357,862]", 4076, 406954)
 
   it "Actual" $ do
     a <- day02 "actual"
-    a `shouldBe` ("[268294,973842]", 0, 98316)
+    a `shouldBe` ("[268294,973842]", 1047, 98316)
