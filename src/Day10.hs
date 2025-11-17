2563e893d9595653d258ee30392cf0eab59825b3
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-17 09:31:54 +0000

    slight refactor for day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -11,32 +11,43 @@ import Data.List (find, inits, tails)
 import Data.Maybe (fromMaybe)
 import ECSolution (Solution, getInput, makeSolution, runDay)
 
+type Dragon = S.HashSet Point2d
+
+type Sheep = S.HashSet Point2d
+
+type Hides = S.HashSet Point2d
+
+type MaxBounds = Point2d
+
+type Part12State = RWS (MaxBounds, Hides) () (Dragon, Sheep) Int
+
+type Part3State = (Point2d, [Point2d])
+
 day10 :: String -> IO (Int, Int, Int)
 day10 = getInput 10 part1 part2 part3
 
 part1 :: String -> Int
-part1 input = sum r
-  where
-    (rdr, initState) = parseInput input
-    -- detect if sample or actual
-    nMoves = if head (S.toList $ fst initState) == (6, 6) then 3 else 4
-    (r, _, _) = runRWS (replicateM nMoves dragonsTurn) rdr initState
+part1 = part12 4 dragonsTurn
 
 part2 :: String -> Int
-part2 input = sum r
-  where
-    (rdr, initState) = parseInput input
-    -- detect if sample or actual
-    nMoves = if head (S.toList $ fst initState) == (6, 6) then 3 else 20
-    (r, _, _) = runRWS (replicateM nMoves turn) rdr initState
+part2 = part12 20 turn
 
-turn :: RWS (Point2d, S.HashSet Point2d) () (S.HashSet Point2d, S.HashSet Point2d) Int
+part12 :: Int -> Part12State -> String -> Int
+part12 n f input = sum r
+  where
+    (rdr, (dragon, sheep)) = parseInputPart input
+    initState = (S.singleton dragon, S.fromList sheep)
+    -- detect if sample or actual
+    nMoves = if head (S.toList $ fst initState) == (6, 6) then 3 else n
+    (r, _, _) = runRWS (replicateM nMoves f) rdr initState
+
+turn :: Part12State
 turn = do
   d <- dragonsTurn
   s <- sheepsTurn
   pure $ d + s
 
-dragonsTurn :: RWS (Point2d, S.HashSet Point2d) () (S.HashSet Point2d, S.HashSet Point2d) Int
+dragonsTurn :: Part12State
 dragonsTurn = do
   maxXY <- fst <$> ask
   modify $ first (S.fromList . concatMap (moveDragon maxXY) . S.toList)
@@ -57,12 +68,12 @@ moveDragon (maxX, maxY) (x, y) = filter inBounds ds
       ]
     inBounds (x, y) = x >= 0 && y >= 0 && x <= maxX && y <= maxY
 
-sheepsTurn :: RWS (Point2d, S.HashSet Point2d) () (S.HashSet Point2d, S.HashSet Point2d) Int
+sheepsTurn :: Part12State
 sheepsTurn = do
   modify $ second (S.map (second succ))
   checkSheep
 
-checkSheep :: RWS (Point2d, S.HashSet Point2d) () (S.HashSet Point2d, S.HashSet Point2d) Int
+checkSheep :: Part12State
 checkSheep = do
   (maxXY, hides) <- ask
   (dragons, sheep) <- get
@@ -73,55 +84,37 @@ checkSheep = do
   put (dragons, sheep')
   pure $ S.size eatenSheep
 
-parseInput :: String -> ((Point2d, S.HashSet Point2d), (S.HashSet Point2d, S.HashSet Point2d))
-parseInput input = ((maxXY, hides), (dragon, sheep))
-  where
-    xs = do
-      (y, line) <- zip [0 ..] (lines input)
-      (x, char) <- zip [0 ..] line
-      pure (char, (x, y))
-    maxXY = maximum $ map snd xs
-    Just dragon = S.singleton . snd <$> find ((== 'D') . fst) xs
-    sheep = S.fromList $ map snd $ filter ((== 'S') . fst) xs
-    hides = S.fromList $ map snd $ filter ((== '#') . fst) xs
-
---- part 3 doesn't reuse any code from part 1 or 2
-
 part3 :: String -> Int
 part3 input = evalState (go initState) M.empty
   where
     --
-    ((maxXY@(_, maxY), hides), initState) = parseInputPart3 input
+    ((maxXY@(_, maxY), hides), initState) = parseInputPart input
     --
-    go :: (Point2d, [Point2d]) -> State (M.HashMap (Point2d, [Point2d]) Int) Int
+    go :: Part3State -> State (M.HashMap Part3State Int) Int
     go (dragon, []) = pure 1
     go k@(dragon, sheep) = do
       cache <- get
       case M.lookup k cache of
         Just v -> pure v
         Nothing -> do
-          let sheep' = if null possibleSheepMoves then [sheep] else possibleSheepMoves
+          let possibleSheepMoves = do
+                (pre, x : post) <- zip (inits sheep) (tails sheep)
+                let x' = second succ x
+                guard $ x' /= dragon || x' `elem` hides
+                pure $ pre ++ (x' : post)
+              sheep' = if null possibleSheepMoves then [sheep] else possibleSheepMoves
               nextStates = possibleDragonMoves dragon sheep'
           v <- sum <$> forM nextStates go
           modify (M.insert k v)
           pure v
-      where
-        possibleSheepMoves :: [[Point2d]]
-        possibleSheepMoves =
-          do
-            (pre, x : post) <- zip (inits sheep) (tails sheep)
-            let x' = second succ x
-            guard $ x' /= dragon || x' `elem` hides
-            pure $ pre ++ (x' : post)
-
-    possibleDragonMoves :: Point2d -> [[Point2d]] -> [(Point2d, [Point2d])]
+    possibleDragonMoves :: Point2d -> [[Point2d]] -> [Part3State]
     possibleDragonMoves dragon sheep = do
       dragon <- moveDragon maxXY dragon
       sheep' <- filter (all ((<= maxY) . snd)) sheep
       pure (dragon, filter ((`elem` hides) <||> (/= dragon)) sheep')
 
-parseInputPart3 :: String -> ((Point2d, S.HashSet Point2d), (Point2d, [Point2d]))
-parseInputPart3 input = ((maxXY, hides), (dragon, sheep))
+parseInputPart :: String -> ((Point2d, S.HashSet Point2d), (Point2d, [Point2d]))
+parseInputPart input = ((maxXY, hides), (dragon, sheep))
   where
     xs = do
       (y, line) <- zip [0 ..] (lines input)
