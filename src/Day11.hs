2c03abaa22f1448cec2e456b3b61aae4ae4d7e44
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-18 10:43:52 +0000

    adds solution for day 11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,16 +1,41 @@
 module Day11 where
 
 import Common
+import Control.Arrow (Arrow (second), (>>>))
 import ECSolution (Solution, getInput, makeSolution, runDay)
 
 day11 :: String -> IO (Int, Int, Int)
 day11 = getInput 11 part1 part2 part3
 
 part1 :: String -> Int
-part1 = const 0
+part1 = run >>> (!! 10) >>> zipWith (*) [1 ..] >>> sum
 
 part2 :: String -> Int
-part2 = const 0
+part2 = run >>> length >>> pred
 
 part3 :: String -> Int
-part3 = const 0
+part3 input = sum r
+  where
+    xs = parseInput input
+    m = sum xs `div` length xs
+    r = [m - x | x <- xs, x < m]
+
+run :: String -> [[Int]]
+run = parseInput >>> goFirst
+  where
+    goFirst xs =
+      let xs' = phase (<=) pred succ xs
+       in if xs == xs' then goSecond xs else xs : goFirst xs'
+    goSecond xs =
+      let xs' = phase (>=) succ pred xs
+       in if xs == xs' then [xs] else xs : goSecond xs'
+    phase :: (Int -> Int -> Bool) -> (Int -> Int) -> (Int -> Int) -> [Int] -> [Int]
+    phase comp nx ny = go
+      where
+        go [x] = [x]
+        go (x : y : xs)
+          | comp x y = x : go (y : xs)
+          | otherwise = nx x : go (ny y : xs)
+
+parseInput :: String -> [Int]
+parseInput = map read . lines
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -6,8 +6,8 @@ spec :: Spec
 spec = describe "Day 11" $ do
   it "Sample" $ do
     s <- day11 "sample"
-    s `shouldBe` (0, 0, 0)
+    s `shouldBe` (109, 1579, 1378)
 
   it "Actual" $ do
     a <- day11 "actual"
-    a `shouldBe` (0, 0, 0)
+    a `shouldBe` (269, 3090294, 135000728971927)
