efba3553ddcf28af50497c95515d3aa331fd5e7a
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-20 00:10:17 +0000

    adds solution for day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -2,15 +2,39 @@ module Day13 where
 
 import Common
 import ECSolution (getInput)
+import Text.Parsec qualified as P
 
 day13 :: String -> IO (Int, Int, Int)
 day13 = getInput 13 part1 part2 part3
 
-part1 :: String -> Int
-part1 = const 0
+part1 = solve 2025
+part2 = solve 20252025
+part3 = solve 202520252025
 
-part2 :: String -> Int
-part2 = const 0
+solve :: Int -> String -> Int
+solve v input = i !! idx
+  where
+    i = buildSequence $ parseInput input
+    n = length i
+    idx = v `mod` n
 
-part3 :: String -> Int
-part3 = const 0
+buildSequence :: [(Int, Int)] -> [Int]
+buildSequence values = 1 : go [] values
+  where
+    go :: [Int] -> [(Int, Int)] -> [Int]
+    go revSeq [] = revSeq
+    go revSeq [ (x1, x2) ] = [x1..x2] ++ revSeq
+    go revSeq ((x1, x2):(y1, y2):xs) = [x1..x2] ++ go ([y2,y2-1..y1]++revSeq) xs
+
+parseInput :: String -> [(Int, Int)]
+parseInput = parse (p `P.sepBy` P.newline)
+  where
+    p = P.try p2 P.<|> p1
+    p2 = do
+      start <- number
+      P.char '-'
+      end <- number'
+      pure (start, end)
+    p1 = do
+      start <- number'
+      pure (start, start)
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -6,8 +6,8 @@ spec :: Spec
 spec = describe "Day 13" $ do
   it "Sample" $ do
     s <- day13 "sample"
-    s `shouldBe` (0, 0, 0)
+    s `shouldBe` (67,30,30)
 
   it "Actual" $ do
     a <- day13 "actual"
-    a `shouldBe` (0, 0, 0)
+    a `shouldBe` (541, 8578, 551167)
