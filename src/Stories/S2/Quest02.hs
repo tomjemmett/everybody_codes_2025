f23d2fd63c541c6bb53833faed5ab367ddd590b8
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-20 20:32:29 +0000

    fix

--- src/Stories/S2/Quest02.hs
+++ src/Stories/S2/Quest02.hs
@@ -10,10 +10,10 @@ import Text.Parsec.String (Parser)
 
 -- expectations:
 s2q2_sample :: (Int, Int, Int)
-s2q2_sample = (7, 2955, 0)
+s2q2_sample = (7, 2955, 2953681)
 
 s2q2_actual :: (Int, Int, Int)
-s2q2_actual = (131, 21497, 0)
+s2q2_actual = (131, 21497, 21199950)
 
 s2q2 :: String -> IO (Int, Int, Int)
 s2q2 = getInput 202 part1 part2 part3
@@ -38,7 +38,7 @@ solve replications input = go (cycle "RGB") lq rq
     lq = S.fromList l
     rq = S.fromList r
     go (y : ys) lq rq
-      | S.null lq = 1
+      | S.null lq = if S.null rq then 0 else 1
       | S.null rq = 1
       | S.length lq == S.length rq && l == y = 1 + go ys lq' rq'
       | S.length lq == S.length rq = 1 + go ys lq' rq
