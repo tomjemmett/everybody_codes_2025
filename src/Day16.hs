9cdd213f60a0a34ac939959e72cd66c9e384272d
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-25 09:38:54 +0000

    refactors day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -7,25 +7,26 @@ day16 :: String -> IO (Int, Int, Int)
 day16 = getInput 16 part1 part2 part3
 
 part1 :: String -> Int
-part1 = sum . take 90 . buildWall . commaSeparatedInts
+part1 = flip countBlocks 90 . commaSeparatedInts
 
 part2 :: String -> Int
 part2 = product . getSpell . commaSeparatedInts
 
 part3 :: String -> Int
-part3 input = binarySearch 1 (t `div` 2)
+part3 = binarySearch . countBlocks . getSpell . commaSeparatedInts
   where
-    xs = getSpell $ commaSeparatedInts input
     t = 202520252025000
-    binarySearch :: Int -> Int -> Int
-    binarySearch low high
-      | low + 1 >= high = low
-    binarySearch low high =
-      if sum (map (mid `div`) xs) < t
-        then binarySearch mid high
-        else binarySearch low mid
+    binarySearch :: (Int -> Int) -> Int
+    binarySearch fn = go 1 (t `div` 2)
       where
-        mid = (low + high) `div` 2
+        go low high
+          | low + 1 >= high = low
+          | r == t = mid
+          | r < t = go mid high
+          | otherwise = go low mid
+          where
+            mid = (low + high) `div` 2
+            r = fn mid
 
 getSpell :: [Int] -> [Int]
 getSpell = go 1
@@ -38,11 +39,5 @@ getSpell = go 1
       where
         xs' = zipWith (-) xs (cycle (replicate (cur - 1) 0 ++ [1]))
 
-buildWall :: [Int] -> [Int]
-buildWall = go (repeat 0)
-  where
-    go :: [Int] -> [Int] -> [Int]
-    go acc [] = acc
-    go acc (x : xs) = go acc' xs
-      where
-        acc' = zipWith (+) (cycle (replicate (x - 1) 0 ++ [1])) acc
+countBlocks :: [Int] -> Int -> Int
+countBlocks xs n = sum $ map (n `div`) xs
