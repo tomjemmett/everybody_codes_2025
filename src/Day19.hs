6f9eaa4b4af37bde1d24b25e239b438e173d55da
Tom Jemmett <thomas.jemmett@nhs.net>
2025-11-27 23:52:37 +0000

    adds solution for day 19

--- src/Day19.hs
+++ src/Day19.hs
@@ -1,16 +1,46 @@
 module Day19 where
 
+import Algorithm.Search
 import Common
+import Control.Monad (guard)
+import Data.HashMap.Strict qualified as M
+import Data.List (sort)
+import Data.Vector qualified as V
 import ECSolution (getInput)
+import Text.Parsec qualified as P
 
 day19 :: String -> IO (Int, Int, Int)
-day19 = getInput 19 part1 part2 part3
+day19 = getInput 19 solve solve solve
 
-part1 :: String -> Int
-part1 = const 0
+solve :: String -> Int
+solve (parseInput -> walls) = case search walls of
+  Just c -> c
+  Nothing -> error "No path found"
 
-part2 :: String -> Int
-part2 = const 0
+search :: M.HashMap Int [(Int, Int)] -> Maybe Int
+search walls = fst <$> dijkstra neighbours cost goal (0, 0)
+  where
+    ks = sort $ M.keys walls
+    maxX = maximum ks
+    goal :: Point2d -> Bool
+    goal (x, _) = x == maxX
+    neighbours :: Point2d -> [Point2d]
+    neighbours (x, y) = concatMap go $ walls M.! x'
+      where
+        x' : _ = dropWhile (<= x) ks
+        dx = x' - x
+        go (l, h) = do
+          y' <- [y - dx, y - dx + 2 .. y + dx]
+          guard $ l <= y' && y' <= h
+          pure (x', y')
+    cost :: Point2d -> Point2d -> Int
+    cost (x1, y1) (x2, y2) = (y2 - y1 + x2 - x1) `div` 2
 
-part3 :: String -> Int
-part3 = const 0
+parseInput :: String -> M.HashMap Int [(Int, Int)]
+parseInput = M.fromListWith (++) . parse (p `P.sepBy` P.newline)
+  where
+    p = do
+      x <- number' <* P.char ','
+      h <- number' <* P.char ','
+      s <- number'
+      pure (x, [(h, h + s)])
--- test/Day19Spec.hs
+++ test/Day19Spec.hs
@@ -6,8 +6,8 @@ spec :: Spec
 spec = describe "Day 19" $ do
   it "Sample" $ do
     s <- day19 "sample"
-    s `shouldBe` (0, 0, 0)
+    s `shouldBe` (24, 22, 22)
 
   it "Actual" $ do
     a <- day19 "actual"
-    a `shouldBe` (0, 0, 0)
+    a `shouldBe` (55, 735, 4558484)
